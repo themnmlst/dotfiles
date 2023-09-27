a83cc6b1121d535b99e02c219a87dfdbc23bba2a
themnmlst <themnmlst.dev@protonmail.com>
2023-09-27 23:01:21 +0200

    neovim config added

---
+++ nvim/.config/nvim/lua/theminimalist/init.lua
@@ -0,0 +1,10 @@
+require("theminimalist.set")
+require("theminimalist.remap")
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
