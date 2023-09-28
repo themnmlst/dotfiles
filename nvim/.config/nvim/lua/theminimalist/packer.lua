d542ba334e204c74f37268acd063a565139adfa9
themnmlst <themnmlst.dev@protonmail.com>
2023-09-28 13:39:04 +0200

    reverted to older treesitter and lsp

--- nvim/.config/nvim/lua/theminimalist/packer.lua
+++ nvim/.config/nvim/lua/theminimalist/packer.lua
@@ -8,7 +8,7 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.3',
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
@@ -44,7 +44,7 @@ return require('packer').startup(function(use)
 
   use {
 	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v3.x',
+	  branch = 'v1.x',
 	  requires = {
 		  -- LSP Support
 		  {'neovim/nvim-lspconfig'},
